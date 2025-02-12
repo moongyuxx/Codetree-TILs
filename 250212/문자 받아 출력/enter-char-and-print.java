07795725b8fa047fb576325b6a9cbd194a650791
Gyuwon Moon <112413876+moongyuxx@users.noreply.github.com>
2025-02-12 14:32:42 +0900

    Title: 문자 받아 출력, Time: 112ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250212/문자 받아 출력/enter-char-and-print.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        char c = input.charAt(0);
+        System.out.print(c);
+        // Please write your code here.
+    }
+}
